c5fc35a0b5777471156a8ef3d6c12c6b871e56d5
hope_fu <hope_fu@163.com>
2016-12-24 12:48:25 +0800

    cat sql operation

--- src/main/java/org/allen/imocker/dao/BaseDao.java
+++ src/main/java/org/allen/imocker/dao/BaseDao.java
@@ -1,5 +1,9 @@
 package org.allen.imocker.dao;
 
+import com.dianping.cat.Cat;
+import com.dianping.cat.CatConstants;
+import com.dianping.cat.message.Message;
+import com.dianping.cat.message.Transaction;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.transaction.annotation.Propagation;
@@ -19,43 +23,83 @@ public abstract class BaseDao {
 
     @Transactional(propagation = Propagation.REQUIRED)
     public Object insert(String sqlId, Object param) {
+        Transaction t = Cat.newTransaction(CatConstants.TYPE_SQL, sqlId);
         try {
-            return sqlMapClient.insert(sqlId, param);
+            Object obj = sqlMapClient.insert(sqlId, param);
+            t.setStatus(Transaction.SUCCESS);
+            Cat.logEvent("SQL.Method", "INSERT", Message.SUCCESS, null);
+            return obj;
         } catch (SQLException e) {
+            t.setStatus(e);
+            Cat.logEvent("SQL.Method", "INSERT", "99", null);
             throw new RuntimeException(String.format("DAO insert failed, sqlId=%s, param=%s, error=%s", sqlId, param, e.getCause().getMessage()), e);
+        } finally {
+            t.complete();
         }
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
     public int update(String sqlId, Object param) {
+        Transaction t = Cat.newTransaction(CatConstants.TYPE_SQL, sqlId);
         try {
-            return sqlMapClient.update(sqlId, param);
+            int ret = sqlMapClient.update(sqlId, param);
+            t.setStatus(Transaction.SUCCESS);
+            Cat.logEvent("SQL.Method", "UPDATE", Message.SUCCESS, null);
+            return ret;
         } catch (SQLException e) {
+            t.setStatus(e);
+            Cat.logEvent("SQL.Method", "UPDATE", "99", null);
             throw new RuntimeException(String.format("DAO update failed, sqlId=%s, param=%s, error=%s", sqlId, param, e.getCause().getMessage()), e);
+        } finally {
+            t.complete();
         }
     }
 
     public <T> List<T> queryForList(String sqlId, Object param) {
+        Transaction t = Cat.newTransaction(CatConstants.TYPE_SQL, sqlId);
         try {
-            return sqlMapClient.queryForList(sqlId, param);
+            List list = sqlMapClient.queryForList(sqlId, param);
+            t.setStatus(Transaction.SUCCESS);
+            Cat.logEvent("SQL.Method", "SELECT", Message.SUCCESS, null);
+            return list;
         } catch (SQLException e) {
+            t.setStatus(e);
+            Cat.logEvent("SQL.Method", "SELECT", "99", null);
             throw new RuntimeException(String.format("DAO queryForList failed, sqlId=%s, param=%s, error=%s", sqlId, param, e.getCause().getMessage()), e);
+        } finally {
+            t.complete();
         }
     }
 
     public <T> T queryForObject(String sqlId, Object param) {
+        Transaction t = Cat.newTransaction(CatConstants.TYPE_SQL, sqlId);
         try {
-            return (T) sqlMapClient.queryForObject(sqlId, param);
+            Object obj = sqlMapClient.queryForObject(sqlId, param);
+            t.setStatus(Transaction.SUCCESS);
+            Cat.logEvent("SQL.Method", "SELECT", Message.SUCCESS, null);
+            return (T) obj;
         } catch (SQLException e) {
+            t.setStatus(e);
+            Cat.logEvent("SQL.Method", "SELECT", "99", null);
             throw new RuntimeException(String.format("DAO queryForObject failed, sqlId=%s, param=%s, error=%s", sqlId, param, e.getCause().getMessage()), e);
+        } finally {
+            t.complete();
         }
     }
 
     public int delete(String sqlId, Object param) {
+        Transaction t = Cat.newTransaction(CatConstants.TYPE_SQL, sqlId);
         try {
-            return sqlMapClient.delete(sqlId, param);
+            int ret = sqlMapClient.delete(sqlId, param);
+            t.setStatus(Transaction.SUCCESS);
+            Cat.logEvent("SQL.Method", "DELETE", Message.SUCCESS, null);
+            return ret;
         } catch (SQLException e) {
+            t.setStatus(e);
+            Cat.logEvent("SQL.Method", "DELETE", "99", null);
             throw new RuntimeException(String.format("DAO delete failed, sqlId=%s, param=%s, error=%s", sqlId, param, e.getCause().getMessage()), e);
+        } finally {
+            t.complete();
         }
     }
 }
