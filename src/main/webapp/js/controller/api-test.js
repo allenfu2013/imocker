8a1afb191b9f86394595cd84ba43c9a4d049ccf5
hope_fu <hope_fu@163.com>
2017-01-11 00:44:05 +0800

    refactor ui

--- src/main/webapp/js/controller/api-test.js
+++ src/main/webapp/js/controller/api-test.js
@@ -8,18 +8,38 @@
         $scope.remoteCallInfo = {};
         $scope.apiInfo.qaUrl = "http://localhost:8081/imocker/api/ping";
         $scope.apiInfo.method = "GET";
-        $scope.postType = null;
+        $scope.postType = "1";
 
         $scope.send = function () {
             $scope.remoteCallInfo.method = $scope.apiInfo.method;
             $scope.remoteCallInfo.url = $scope.apiInfo.qaUrl;
 
-            console.log($scope.postType);
-
-            /*$http.post("api/postman", $scope.remoteCallInfo).success(function (ret) {
+            $http.post("api/postman", $scope.remoteCallInfo).success(function (ret) {
                 $("#callResult").val(ret);
-            });*/
+            });
         };
+
+        $scope.addHeaders = function () {
+            console.log("add headers");
+            var headerModel = "<div style='margin-top: 10px;'><input type='text' class='form-control' style='width:200px;display: inline;' placeholder='key'><input type='text' class='form-control' style='width:200px;display: inline;' placeholder='value'> <button class='btn btn-default glyphicon glyphicon-minus header-minus'></button></div>";
+            $("#headersContainer").append(headerModel);
+            $(".header-minus").unbind("click");
+            $(".header-minus").click(function () {
+                $(this).parent().remove();
+            });
+        };
+
+        $scope.addParams = function ($compile) {
+            console.log("add params");
+            var paramTemplate = "<div style='margin-top: 10px;'><input type='text' class='form-control' style='width:200px;display: inline;' placeholder='key'><input type='text' class='form-control' style='width:200px;display: inline;' placeholder='value'> <button class='btn btn-default glyphicon glyphicon-minus param-minus'></button></div>";
+            $("#paramsContainer").append(paramTemplate);
+            $(".param-minus").unbind("click");
+            $(".param-minus").click(function () {
+                $(this).parent().remove();
+            });
+        };
+
+
     });
 
 }());
