6b3b2813011ade659b54cc26bce6986c8e7ea69b
allenfu2013 <hope_fu@163.com>
2018-04-15 21:17:55 +0800

    add druid

--- src/main/java/org/allen/imocker/Application.java
+++ src/main/java/org/allen/imocker/Application.java
@@ -2,11 +2,8 @@ package org.allen.imocker;
 
 import org.allen.imocker.httpclient.HttpRemoteService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
